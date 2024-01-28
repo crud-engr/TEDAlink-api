6b7f4b0086e56c0d28b5227cfa62f61675928f1d
crud-engr <abeebayinla@gmail.com>
2024-01-28 02:19:36 +0100

    Admission application and update password

---
+++ src/parent/model/Admission.js
@@ -0,0 +1,80 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const AdmissionSchema = new Schema(
+  {
+    parentId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Parent',
+    },
+    schoolId: {
+      type: Schema.Types.ObjectId,
+      ref: 'School',
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    dob: {
+      type: String,
+      required: true,
+    },
+    dob: {
+      type: String,
+      required: true,
+    },
+    placeOfBirth: {
+      type: String,
+      required: true,
+    },
+    applicantClass: {
+      type: String,
+      required: true,
+    },
+    residentAddress: {
+      type: String,
+      required: true,
+    },
+    presentSchool: {
+      type: String,
+      required: true,
+    },
+    postalCode: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    lga: {
+      type: String,
+      required: true,
+    },
+    relationshipToPupil: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+module.exports = mongoose.model('Admission', AdmissionSchema);
+
+AdmissionSchema.set('toJSON', {
+  transform: (doc, ret) => {
+    delete ret.__v;
+    return ret;
+  },
+});
