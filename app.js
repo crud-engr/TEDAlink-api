bfd89b6af4da5759144b327fc5feab0b2b266f35
crud-engr <abeebayinla@gmail.com>
2024-02-04 07:46:47 +0100

    Disabled rate limit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const compression = require('compression');
 const helmet = require('helmet');
 const moment = require('moment');
-const rateLimit = require('express-rate-limit');
+// const rateLimit = require('express-rate-limit');
 const xss = require('xss-clean');
 const mongoSanitize = require('express-mongo-sanitize');
 const fileUpload = require('express-fileupload');
@@ -24,14 +24,14 @@ app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('combined'));
 
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 24 * 60 * 60 * 1000, // 24 hours
-  message:
-    'We received too many requests from this IP. Please try again in 24hrs to this time',
-});
+// const limiter = rateLimit({
+//   max: 100,
+//   windowMs: 24 * 60 * 60 * 1000, // 24 hours
+//   message:
+//     'We received too many requests from this IP. Please try again in 24hrs to this time',
+// });
 
-app.use('/api', limiter);
+// app.use('/api', limiter);
 app.use(helmet());
 app.use(mongoSanitize());
 app.use(compression());
